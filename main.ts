3125abdcc57efa7e7af3055c3557201422d78fe3
Caleb Ukle <me+info@calebukle.com>
2022-07-14 20:28:33 -0500

    add canonical url fix

--- main.ts
+++ main.ts
@@ -1,5 +1,6 @@
 import blog, { redirects } from "https://deno.land/x/blog@0.4.1/blog.tsx";
-import { info } from './middleware/info.ts';
+import { canonicalUrls } from "./middleware/canonicalUrls.ts";
+
 blog({
   title: "Caleb Ukle",
   description: "This is where I write about random stuff. Enjoy.",
@@ -12,19 +13,18 @@ blog({
     { title: "Twitter", url: "https://twitter.com/cu_galaxy" },
     { title: "GitHub", url: "https://github.com/barbados-clemens" },
   ],
-  style: '',
-  theme: 'auto',
+  theme: "auto",
   middlewares: [
+    // If you want to set up Google Analytics, paste your GA key here.
+    // ga("UA-XXXXXXXX-X"),
 
-  // If you want to set up Google Analytics, paste your GA key here.
-  // ga("UA-XXXXXXXX-X"),
-
-  // If you want to provide some redirections, you can specify them here,
-  // pathname specified in a key will redirect to pathname in the value.
-  redirects({
-   "/hello_world.html": "/hello_world",
-   "/blog": "/"
-  }),
-  info({blah: 'abc'})
-  ]
+    // If you want to provide some redirections, you can specify them here,
+    // pathname specified in a key will redirect to pathname in the value.
+    canonicalUrls({ baseUrl: "https://calebukle.com" }),
+    redirects({
+      "/hello_world.html": "/hello_world",
+      "/blog": "/?tag=Blog",
+      "/lego": "/?tag=Lego",
+    }),
+  ],
 });
