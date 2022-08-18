6674743a592c09657c1721d94dd4487ac793d01b
Caleb Ukle <me+info@calebukle.com>
2022-08-18 17:10:21 -0500

    throw if no token

--- utils/discogs.ts
+++ utils/discogs.ts
@@ -3,31 +3,36 @@ function rand(max: number) {
 }
 
 export async function getImages(): Promise<[string, string, string, string]> {
-  const init = {
-    headers: {
-      "user-agent": `CalebUkleCom Deno/${Deno.version.deno}`,
-      "Authorization": `Discogs token=${Deno.env.get("DISCOGS_TOKEN")}`,
-    },
-  };
+  try {
 
-  const collection = await fetch(
-    "https://api.discogs.com/users/Barbados_Clemens/collection/folders/0/releases?per_page=200",
-    init,
-  ).then((r) => r.json());
+    const init = {
+      headers: {
+        "user-agent": `CalebUkleCom Deno/${Deno.version.deno}`,
+        "Authorization": `Discogs token=${Deno.env.get("DISCOGS_TOKEN")}`,
+      },
+    };
+    const collection = await fetch(
+      "https://api.discogs.com/users/Barbados_Clemens/collection/folders/0/releases?per_page=200",
+      init,
+    ).then((r) => r.json())
 
-  const n = collection.releases.length;
+    const n = collection.releases.length;
 
-  const selected = [
-    collection.releases[rand(n)],
-    collection.releases[rand(n)],
-    collection.releases[rand(n)],
-    collection.releases[rand(n)],
-  ];
+    const selected = [
+      collection.releases[rand(n)],
+      collection.releases[rand(n)],
+      collection.releases[rand(n)],
+      collection.releases[rand(n)],
+    ];
 
-  return selected.map((s) => s.basic_information.cover_image) as [
-    string,
-    string,
-    string,
-    string,
-  ];
+    return selected.map((s) => s.basic_information.cover_image) as [
+      string,
+      string,
+      string,
+      string,
+    ];
+  } catch (e) {
+    console.log(e);
+    return ["", "", "", ""];
+  }
 }
